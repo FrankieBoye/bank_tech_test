462a1ea84eb2ea6b3ed3e7fd07546f9e85bc5589
FrankieBoye <frankie4@btinternet.com>
2019-06-06 12:01:11 +0100

    individual debit and credit transactions passed into transaction array

--- lib/account.rb
+++ lib/account.rb
@@ -2,18 +2,21 @@
 
 # main class
 class Account
-  attr_reader :balance
+  attr_reader :balance, :transaction, :date
 
   def initialize
     @balance = 0
+    @date = Time.now.strftime('%d/%m/%y')
+    @transaction = []
   end
 
   def credit(credit_amount)
     @balance += credit_amount
+    @transaction << "#{@date} || #{credit_amount} || || #{@balance}"
   end
 
-def debit(debit_amount)
-  @balance -= debit_amount
-end
-
+  def debit(debit_amount)
+    @balance -= debit_amount
+    @transaction << "#{@date} || || #{debit_amount} || #{@balance}"
+  end
 end
